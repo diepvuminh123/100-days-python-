fb3dabafdfcb12ba91cd43d8e8381bae9aec2c5b
Minhdev2004 <chinhtpc123@gmail.com>
2024-05-07 18:31:07 +0700

    Day1

---
+++ Day1.py
@@ -0,0 +1,8 @@
+#print hello world
+print("Hello world")
+#print the text " she say "Hello" after that left
+print("she say \Hello\ after that left")
+# print Hello + world 
+print("Hello" + " world")
+# print Hello world 5 times
+print("Hello world " * 5)
