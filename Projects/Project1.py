9aff0f454e829c580ac09a76b2e7a4ca2cb78bad
Minhdev2004 <chinhtpc123@gmail.com>
2024-05-08 18:59:20 +0700

    add comment project 1 project2

--- Projects/Project1.py
+++ Projects/Project1.py
@@ -1,5 +1,12 @@
-print("welcome to the band name generator")
-city=input("What's the name of the city you grew up in?\n")
-pet=input("What's your pet's name?\n")
-print("Your band name could be"+" "+city+" "+pet)
 
+# This program is a band name generator
+print("welcome to the band name generator")
+
+# Ask the user for the name of the city they grew up in
+city=input("What's the name of the city you grew up in?\n")
+
+# Ask the user for their pet's name
+pet=input("What's your pet's name?\n")
+
+# Combine the city and pet name to generate a potential band name
+print("Your band name could be"+" "+city+" "+pet)
--- Projects/Project2.py
+++ Projects/Project2.py
@@ -1,17 +1,25 @@
+# Print welcome message and prompt user for input
 print("Welcome to the tip calculator!")
-totalbill= input("What was the total bill? $")
-tip=input("How much tip would you like to give? 10, 12, or 15?")
-people=input("How many people to split the bill?")
-#money each person should pay 
-money1=float(totalbill)/ int(people) 
-money= round(money1 , 2)
-print(money) #Test
-#tip round 2 decimal
-tip1=int(tip) / 100 
-print(tip1) #Test 
-#finalmoney people should pay 
-finalmoney=round((money * tip1)+money1,2)
-print(finalmoney) #Test 
-print("Each person should pay:" + str(finalmoney)) 
+totalbill = input("What was the total bill? $")
+tip = input("How much tip would you like to give? 10, 12, or 15?")
+people = input("How many people to split the bill?")
+
+# Calculate the amount of money each person needs to pay
+money1 = float(totalbill) / int(people)  # Divide the total bill by the number of people
+money = round(money1, 2)  # Round to 2 decimal places
+print(money)  # Test the calculation result
+
+# Calculate the tip amount based on the percentage entered
+tip1 = int(tip) / 100
+print(tip1)  # Test the calculation result
+
+# Calculate the total amount each person needs to pay after adding the tip
+finalmoney = round((money * tip1) + money1, 2)
+print(finalmoney)  # Test the calculation result
+
+# Print the amount each person needs to pay
+print("Each person should pay: " + str(finalmoney))  # Use string concatenation
+print(f"Each person should pay: ${finalmoney}")  # Use f-string to insert the value into the string
+
 
 
