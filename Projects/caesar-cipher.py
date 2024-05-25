87399b5cad33b27b9e7dae9ba6555ca7000efb64
Minhdev2004 <chinhtpc123@gmail.com>
2024-05-26 00:02:00 +0700

    finish shiftright

--- Projects/caesar-cipher.py
+++ Projects/caesar-cipher.py
@@ -1,5 +1,25 @@
 import string
 
-alphabet = string.ascii_lowercase
-print("Type 'encode' to encrypt, type 'decode' to decrypt:")
-print(alphabet[1])
+def shiftright(letter,shift):
+ alphabet = string.ascii_letters
+ index=alphabet.index(letter)
+ finalleter=alphabet[(index+shift)%26]
+
+
+ return finalleter
+        
+# print("Type 'encode' to encrypt, type 'decode' to decrypt:")
+# a=input()
+# if a=="encode": 
+#     print("Type the sentence: \n")
+#     sentence=input()
+#     print("type the shift: \n")
+#     shift=int(input())
+a=shiftright("z",1)
+print(a)
+
+
+
+
+
+
--- Test.py
+++ Test.py
@@ -1,8 +1,15 @@
-#Test lower
-a=input().lower()
-print(str(a))
-#Test while not 
-x = 0
-while not x == 5:
-    print(x)
-    x += 1
+# #Test lower
+# a=input().lower()
+# print(str(a))
+# #Test while not 
+# x = 0
+# while not x == 5:
+#     print(x)
+#     x += 1
+
+
+c=input()
+for i in c: 
+    d=c[i+1]
+
+
