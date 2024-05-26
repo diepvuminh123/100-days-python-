e739f571a2304977b422bcc73a947efb9e4327ad
Minhdev2004 <chinhtpc123@gmail.com>
2024-05-26 13:46:59 +0700

    test encode

--- Projects/caesar-cipher.py
+++ Projects/caesar-cipher.py
@@ -1,22 +1,31 @@
 import string
 
-def shiftright(letter,shift):
+def shiftright(letter,shift): # shift right 
  alphabet = string.ascii_letters
  index=alphabet.index(letter)
  finalleter=alphabet[(index+shift)%26]
-
-
  return finalleter
-        
-# print("Type 'encode' to encrypt, type 'decode' to decrypt:")
-# a=input()
-# if a=="encode": 
-#     print("Type the sentence: \n")
-#     sentence=input()
-#     print("type the shift: \n")
-#     shift=int(input())
-a=shiftright("z",1)
-print(a)
+def shiftleft(letter ,shift):   #shift left 
+ alphabet = string.ascii_letters
+ index=alphabet.index(letter)
+ finalleter=alphabet[(index-shift)%26]
+ return finalleter
+
+final=[]
+print("Type 'encode' to encrypt, type 'decode' to decrypt:")
+a="encode"
+if a=="encode": 
+    print("Type the sentence: \n")
+    sentence="abc"
+    print("type the shift: \n")
+    shift=2
+    for i in sentence:
+      j=0
+      final+=shiftright(i,shift)
+# a=shiftleft("a",1) // Test 
+
+print("".join(final))
+# print(a) // Test 
 
 
 
