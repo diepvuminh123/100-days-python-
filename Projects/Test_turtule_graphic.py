f8467c46be2b31311571ef99eb56ba335487e09c
Minhdev2004 <chinhtpc123@gmail.com>
2024-06-02 23:50:52 +0700

    done

--- Projects/Test_turtule_graphic.py
+++ Projects/Test_turtule_graphic.py
@@ -1,5 +1,14 @@
-from turtle import turtles
-turtle1= turtles()
-print(turtles)
+from turtle import Turtle , Screen
+turtle1= Turtle()
+turtle1.shape("turtle")
+turtle1.forward(100)
+turtle1.left(120)
+turtle1.forward(100)
+turtle1.home()
+turtle1.color("blue")
+print(Turtle)
+myscreen=Screen()
+print(myscreen.canvheight)
+myscreen.exitonclick()
 
 
